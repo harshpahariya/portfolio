cc9b79ff8df2e574d38de4cdc0e9e45724531c04
Harsh Pahariya <harsh.pahariya78@gmail.com>
2020-04-03 11:22:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Tags from './components/Tags';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Tags />
+
+    </div>
+  );
+}
+
+export default App;
