2056a3470a3bff55352e8961ba3bf46413260317
Harsh Pahariya <harsh.pahariya78@gmail.com>
2021-05-22 22:55:18 +0530

    Info Design changes

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react'
-import { Image } from 'antd'
+import { Divider, Image,Card } from 'antd'
 import john from './../../assets/john.png'
 import avatar from './../../assets/avatar.png'
 import { GithubOutlined, LinkedinOutlined, TwitterOutlined, FacebookOutlined, InstagramOutlined } from '@ant-design/icons'
-import { Tooltip } from 'antd';
+import { Tooltip, Typography } from 'antd';
+const { Title,Text } = Typography;
 
 class Info extends Component {
     render() {
         return (
             <>
-                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+                <Card>
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
                     <Image width={200} height={200} src={john} fallback={avatar} />
-                    <div style={{ textAlign: 'center' }}>
-                        <h1>Hi, I am John Doe</h1>  <br />
-                        <h3>Full stack developer</h3>
+                    <div style={{marginLeft: '10%'}}>
+                        <Title style={{marginBottom:'5px'}}>Hi, I am John Doe</Title>
+                        <Text style={{fontSize:'25px'}} type="secondary" underline>Full stack developer</Text><br /><br/>
+                        <Text type="secondary" style={{fontSize:'20px'}}>Experienced full stack developer with demonstrated history of working in IT /ITeS Companies.</Text>
                     </div>
-                </div>
-                <div style={{left:'40%', position:'absolute', bottom:'0', marginBottom:'10vh'}}>
-                    <h4 style={{textAlign:'center'}}>Contact me</h4>
-                    <div style={{ marginTop: '8vh' }}>
+                </div> 
+                </Card>
+                <Divider />
+                <div style={{display:'flex',flexDirection:'row'}}>
+                    <div style={{marginTop:'10vh'}}><Text style={{fontSize:'30px'}} type="secondary" underline>Contact me</Text></div>
+                    <div style={{ marginLeft: '14%'}}>
+                        
                         <Tooltip title="github">
                             <GithubOutlined style={{ fontSize: '2.5em', margin: '10px', padding: '10px' }} />
                         </Tooltip>
@@ -36,6 +42,7 @@ class Info extends Component {
                         </Tooltip>
                     </div>
                 </div>
+                
             </>
         )
     }
